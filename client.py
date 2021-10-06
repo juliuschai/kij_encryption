f4e9d385b2312ef1ca26e267fbe9b981ea7d3e1a
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2021-10-06 17:21:21 +0700

    broken push
    need to move to other OS

--- client.py
+++ client.py
@@ -1,25 +1,25 @@
 from ctypes import sizeof
-from os import read, sched_setscheduler
+from os import SEEK_CUR, read, sched_setscheduler
 import os
 import socket
 import base64
 import blowfish
-
-ADDRESS = 'localhost'
-PORT = 10001
-BUFFER_SIZE = 1024
-key = '0123456789abcdef'
+from Crypto.PublicKey import RSA
 
 class connection:
     def __init__(self):
+        self.ADDRESS = 'localhost'
+        self.PORT = 10001
+        BUFFER_SIZE = 1024
+        self.key = b''
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.Connect()
         
     def Connect(self):
-        self.sock.connect((ADDRESS,PORT))
-        self.Send()
-    
-    def Send(self):
+        self.sock.connect((self.ADDRESS,self.PORT))
+        self.SendFile()
+        
+    def Send(self,message):
         with open('/home/feinard/Documents/GitHub/AES-Implementation/civic_renewal_forms.zip','rb') as infile:
             while True:
                 chunk_data = infile.read(64)
@@ -27,15 +27,26 @@ class connection:
                     break
                 elif len(chunk_data) == 64:
                     chunk_data = self.Encryptor(chunk_data)
-                self.sock.sendall(chunk_data)
+                self.sock.SendFileall(chunk_data)
             infile.close()
         self.Command()
+    
+    def SendFile(self):
+        return self.sock.sendall(message.encode())
     
     def Receive(self):
         return
     
+    def KeyGenerator(self):
+        if self.key == b'':
+            self.key = RSA.generate(40)
+            message = "key {}" . format(self.key)
+            self.Send(message)
+
+        return key
+    
     def Encryptor(self,data):
-        cipher = blowfish.Cipher(b'Key must be between 4 and 56 bytes long.')
+        cipher = blowfish.Cipher(bytes(self.key, 'utf-8'))
         return b"".join(cipher.encrypt_ecb(data))
     
     def Command(self):
--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import socket
 import logging
 import blowfish
+from Crypto.PublicKey import RSA
 
 server_address = ('localhost',10001)
 BUFFER_SIZE = 64
-key = b'iloveprogramming'
 
 
 class connection:
     def __init__(self):
+        self.key = b''
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.sock.bind((server_address))
         self.sock.listen()
@@ -19,32 +20,51 @@ class connection:
             conn, addr  = self.sock.accept()
             with conn:
                 logging.warning('Connected :',addr)
-                self.Receive(conn)
-                   
+                self.Retrieve(conn)
                 self.sock.close()
                 return
     
     def Send(self):
         return
     
-    def Receive(self,conn):
+    def ReceiveData(self,conn):
         with open('Output.zip','wb') as file:
             while True:
                 data = conn.recv(BUFFER_SIZE)
                 # self.Command(data)
                 if len(data) == 0:
-                    return
+                    return self.Retrieve(conn)
                 if len(data) == 64:
                     data = self.Decypher(data)
                 file.write(data)
             
         return
     
-    def Retrieve(self):
-        return
+    def Retrieve(self,conn):
+        while True:
+            data =  conn.recv(BUFFER_SIZE).decode()
+            j = data.split(" ")
+            try:
+                command = j[0].strip()
+                if(command == "key"):
+                    return self.KeyRetriever(command[1].strip(),conn)
+                elif(command == "data"):
+                    return self.ReceiveData(conn)
+                
+            except IndexError:
+                return "-Command Error"
+            
+    
+    def KeyRetriever(self,key,conn):
+        if self.key == b'':
+            self.key = RSA.import_key(key)
+            return self.Retrieve(conn)
+        else:
+            return self.key
+        
     
     def Decypher(self,data):
-        cipher = blowfish.Cipher(b'Key must be between 4 and 56 bytes long.')
+        cipher = blowfish.Cipher()
         return b"".join(cipher.decrypt_ecb(data))
         
 if __name__ == '__main__':
