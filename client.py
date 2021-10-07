4c0307a1a8191d6529050bf6d9a25fbfeb5e2a66
FeinardSlim <58687783+FeinardSlim@users.noreply.github.com>
2021-10-07 14:22:48 +0700

    problem with sending bytes

--- client.py
+++ client.py
@@ -1,60 +1,77 @@
 from ctypes import sizeof
-from os import SEEK_CUR, read, sched_setscheduler
-import os
+from os import EX_PROTOCOL
 import socket
-import base64
-import blowfish
+from Crypto import PublicKey
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import Blowfish, PKCS1_OAEP
 
 class connection:
     def __init__(self):
         self.ADDRESS = 'localhost'
-        self.PORT = 10001
-        BUFFER_SIZE = 1024
+        self.PORT = 10000
+        self.BUFFER_SIZE = 2048
         self.key = b''
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.Connect()
         
     def Connect(self):
+        print("Connecting........")
         self.sock.connect((self.ADDRESS,self.PORT))
-        self.SendFile()
+        
+        print("Connect Success!")
+        self.Command()
         
     def Send(self,message):
-        with open('/home/feinard/Documents/GitHub/AES-Implementation/civic_renewal_forms.zip','rb') as infile:
+        return self.sock.sendall(message.encode())
+       
+    
+    def SendFile(self):
+        print("Sending File.........")
+        self.Send('data')
+        with open('civic_renewal_forms.zip','rb') as infile:
             while True:
                 chunk_data = infile.read(64)
                 if(len(chunk_data) == 0):
                     break
                 elif len(chunk_data) == 64:
                     chunk_data = self.Encryptor(chunk_data)
-                self.sock.SendFileall(chunk_data)
+                self.sock.send(chunk_data)
             infile.close()
         self.Command()
     
-    def SendFile(self):
-        return self.sock.sendall(message.encode())
+    def RSARetrivever(self,RSAvalue):
+        self.private_key =  RSA.import_key(RSAvalue)
+        self.private_key = PKCS1_OAEP.new(self.private_key)
+        return self.Command()
     
-    def Receive(self):
-        return
-    
-    def KeyGenerator(self):
-        if self.key == b'':
-            self.key = RSA.generate(40)
-            message = "key {}" . format(self.key)
-            self.Send(message)
-
-        return key
+    def keyRetriever(self):
+        key = self.sock.recv(self.BUFFER_SIZE)
+        self.key = self.private_key.decrypt(key)
+        return self.SendFile()
     
     def Encryptor(self,data):
-        cipher = blowfish.Cipher(bytes(self.key, 'utf-8'))
-        return b"".join(cipher.encrypt_ecb(data))
+        Cipher = Blowfish.new(self.key,Blowfish.MODE_ECB)
+        return b"".join(Cipher.encrypt(data))
     
     def Command(self):
         while True:
+            data =  self.sock.recv(self.BUFFER_SIZE)
+            data = data.decode()
+            j = data.split('\r\n\r\n')
+            command = j[0].strip()
+            if(command == "RSA"):
+                print("Getting RSA.........")
+                self.RSARetrivever(j[1])
+                print("RSA Receive.........")
+            if command == "key":
+                print("Getting Key.........")
+                self.keyRetriever()
+                print("Key Receive.........")
             return
         
     
 if __name__ == '__main__':
     connect = connection()
+    
     
     
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
+from os import SEEK_CUR, wait
 import socket
 import logging
-import blowfish
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, Blowfish
 
-server_address = ('localhost',10001)
+server_address = ('localhost',10000)
 BUFFER_SIZE = 64
 
 
@@ -17,55 +18,63 @@ class connection:
     
     def Connect(self):
         while True:
-            conn, addr  = self.sock.accept()
-            with conn:
-                logging.warning('Connected :',addr)
-                self.Retrieve(conn)
+            self.conn, self.addr  = self.sock.accept()
+            with self.conn:
+                logging.warning('Connected :',self.addr)
+                self.Command()
                 self.sock.close()
                 return
     
-    def Send(self):
-        return
+    def Send(self,command,message):
+        message = b"\r\n\r\n".join([command.encode('utf-8'),message])
+        print(message)
+        return self.conn.sendall(message)
     
-    def ReceiveData(self,conn):
+    def ReceiveData(self):
         with open('Output.zip','wb') as file:
             while True:
-                data = conn.recv(BUFFER_SIZE)
+                data = self.conn.recv(BUFFER_SIZE)
                 # self.Command(data)
                 if len(data) == 0:
-                    return self.Retrieve(conn)
+                    return self.Retrieve(self.conn)
                 if len(data) == 64:
                     data = self.Decypher(data)
                 file.write(data)
-            
-        return
     
-    def Retrieve(self,conn):
+    def Command(self):
+        print("Sending RSA and Key.........")
+        self.KeyGenerator()
         while True:
-            data =  conn.recv(BUFFER_SIZE).decode()
+            data =  self.conn.recv(BUFFER_SIZE).decode()
             j = data.split(" ")
-            try:
-                command = j[0].strip()
-                if(command == "key"):
-                    return self.KeyRetriever(command[1].strip(),conn)
-                elif(command == "data"):
-                    return self.ReceiveData(conn)
-                
-            except IndexError:
-                return "-Command Error"
+            command = j[0].strip()
+            if(command == "data"):
+                return self.ReceiveData(self.conn)
             
     
-    def KeyRetriever(self,key,conn):
-        if self.key == b'':
-            self.key = RSA.import_key(key)
-            return self.Retrieve(conn)
-        else:
-            return self.key
+    def KeyGenerator(self):
+        key = RSA.generate(2048)
+        private_key = key.export_key('PEM')
+        public_key = key.public_key().export_key('PEM')
+        
+        self.Send('RSA',private_key)
+        
+        self.key = str.encode("this is example text")
+        
+        
+        rsa_public_key = RSA.import_key(public_key)
+        rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
+        
+        self.encrypt_key = rsa_public_key.encrypt(self.key)
+        
+        self.Send('key',self.encrypt_key)
+        
+        return 
         
     
     def Decypher(self,data):
-        cipher = blowfish.Cipher()
-        return b"".join(cipher.decrypt_ecb(data))
+        cipher = Blowfish.new(self.key,Blowfish.MODE_ECB)
+        return b"".join(cipher.decrypt(data))
         
 if __name__ == '__main__':
     con = connection()
